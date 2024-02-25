df43e5b8d2ad8b696ba4e9466430e2a774fa5da6
DhairyaParikh31 <dhairyaparikh31@gmail.com>
2024-02-25 17:19:03 +0530

    Dynamic UI according to change in weather

--- app/src/main/java/com/dhairyaparikh/cloudpoint/MainActivity.kt
+++ app/src/main/java/com/dhairyaparikh/cloudpoint/MainActivity.kt
@@ -2,11 +2,134 @@ package com.dhairyaparikh.cloudpoint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.SearchView
+import com.dhairyaparikh.cloudpoint.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 //5b7bca815addf0b420ba8523491dc049
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        fetchWeatherData("Delhi")
+        SearchCity()
+    }
+
+    private fun SearchCity() {
+        val searchView = binding.searchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    fetchWeatherData(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+    }
+
+    private fun fetchWeatherData(cityName : String) {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+        val response = retrofit.getWeatherData(cityName,"5b7bca815addf0b420ba8523491dc049","metric")
+        response.enqueue(object  : Callback<CloudPoint>{
+            override fun onResponse(call: Call<CloudPoint>, response: Response<CloudPoint>) {
+                val responseBody = response.body()
+                if(response.isSuccessful && responseBody!=null){
+                    val temperature = responseBody.main.temp
+                    val humidity=responseBody.main.humidity
+                    val windSpeed = responseBody.wind.speed
+                    val sunRise = responseBody.sys.sunrise.toLong()
+                    val sunSet = responseBody.sys.sunset.toLong()
+                    val seaLevel = responseBody.main.pressure
+                    val condition = responseBody.weather.firstOrNull()?.main?:"unknown"
+                    val maxTemp = responseBody.main.temp_max
+                    val minTemp = responseBody.main.temp_min
+
+
+                    binding.Temp.text= "$temperature °C"
+                    binding.sunrise.text = "${time(sunRise)}  AM"
+                    binding.sunset.text="${time(sunSet)}  PM"
+                    binding.humidity.text="$humidity %"
+                    binding.condition.text="$condition"
+                    binding.weather.text="$condition"
+                    binding.Max.text="Max Temp: $maxTemp °C"
+                    binding.Min.text="Min Temp: $minTemp °C"
+                    binding.sea.text="$seaLevel hPa"
+                    binding.windspeed.text="$windSpeed m/s"
+                    binding.City.text="${cityName.capitalize()}"
+                    binding.Date.text=date()
+                    binding.Day.text=dayName(System.currentTimeMillis())
+
+                    changeImgAccToW(condition)
+                }
+            }
+
+            override fun onFailure(call: Call<CloudPoint>, t: Throwable) {
+
+            }
+
+        })
+
+        }
+
+    private fun changeImgAccToW(conditions: String) {
+        when(conditions){
+            "Haze","Partly Clouds","Clouds","Overcast","Mist","Foggy" ->{
+                binding.root.setBackgroundResource(R.drawable.colud_background)
+                binding.lottieAnimationView.setAnimation(R.raw.cloud)
+            }
+            "Clear Sky","Sunny","Clear" ->{
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+            "Light Rain","Drizzle","Moderate Rain","Showers","Heavy Rain","Rain","Raining" ->{
+                binding.root.setBackgroundResource(R.drawable.rain_background)
+                binding.lottieAnimationView.setAnimation(R.raw.rain)
+            }
+            "Light Snow","Blizzard","Moderate Snow","Heavy Snow","Snow","Snowing" ->{
+                binding.root.setBackgroundResource(R.drawable.snow_background)
+                binding.lottieAnimationView.setAnimation(R.raw.snow)
+            }
+            else ->{
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+
+        }
+        binding.lottieAnimationView.playAnimation()
+    }
+
+    private fun date(): String {
+        val sdf =SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        return sdf.format(Date())
+    }private fun time(timestamp: Long): String {
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        return sdf.format(Date(timestamp*1000))
+    }
+
+    fun dayName(timestamp: Long): String{
+        val sdf =SimpleDateFormat("EEEE", Locale.getDefault())
+        return sdf.format(Date())
     }
 }
